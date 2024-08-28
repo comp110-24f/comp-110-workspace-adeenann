e5eabdbd64d1e54fc58e7dafedc1540bc39cb302
Adeenann <adeenan@ad.unc.edu>
2024-08-28 17:53:25 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,1 +1,8 @@
-print("hello")
+"""My first exercise in COMP110!"""
+
+__author__ = "730761631"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition"""
+    return "Hello, " + name + "!"
